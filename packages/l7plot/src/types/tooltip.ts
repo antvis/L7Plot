826b3babc0be1f4974a385a50743c51e184b585c
yunji <yunji.me@outlook.com>
2021-07-01 15:49:25 +0800

    wip: tooltip interface

--- packages/l7plot/src/types/tooltip.ts
+++ packages/l7plot/src/types/tooltip.ts
@@ -1,4 +1,11 @@
+import { anchorType } from '@antv/l7-utils';
 export interface ITooltip {
-  name?: string;
-  lngLat?: number[];
+  title?: string;
+  showTitle?: boolean;
+  trigger?: 'mousemove' | 'click';
+  className: string;
+  anchor: anchorType[any];
+  offsets: number[];
+  maxWidth: string;
+  customContent?: (data: any) => string;
 }
