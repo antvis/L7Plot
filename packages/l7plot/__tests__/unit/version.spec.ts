4715a1dd73bc5bfbab43da1dde1d4ac3c8bce66f
yunji <yunji.me@outlook.com>
2021-07-12 20:13:16 +0800

    test: update test version describe

--- packages/l7plot/__tests__/unit/version.spec.ts
+++ packages/l7plot/__tests__/unit/version.spec.ts
@@ -1,7 +1,7 @@
 import { version } from '../../src';
 import pkg from '../../package.json';
 
-describe('version', () => {
+describe('l7plot version', () => {
   it('should match the `version` field of package.json', () => {
     const expected = pkg.version;
     expect(version).toBe(expected);
