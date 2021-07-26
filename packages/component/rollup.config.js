ab5143152ec534c02e949c33173a130558589beb
yunji <yunji.me@outlook.com>
2021-07-26 11:57:22 +0800

    wip: umd pack

--- packages/component/rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/umd/l7plot-component.js',
-    format: 'umd',
-    name: 'l7plot-component',
-  },
-  plugins: [],
-};
--- packages/l7plot/rollup.config.js
+++ packages/l7plot/rollup.config.js
@@ -1,9 +1,39 @@
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import analyze from 'rollup-plugin-analyzer';
+// import { terser } from 'rollup-plugin-terser';
+
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/umd/l7plot.js',
-    format: 'umd',
-    name: 'l7plot',
-  },
-  plugins: [],
+  output: [
+    // {
+    //   file: 'dist/umd/l7plot.min.js',
+    //   format: 'umd',
+    //   name: 'l7plot',
+    //   sourcemap: false,
+    //   plugins: [terser()],
+    // },
+    {
+      file: 'dist/umd/l7plot.js',
+      format: 'umd',
+      name: 'l7plot',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    nodePolyfills(),
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    commonjs({
+      // dynamicRequireTargets: ['node_modules/inversify/lib/syntax/binding_{on,when}_syntax.js'],
+    }),
+    typescript(),
+    analyze({
+      summaryOnly: true,
+      limit: 50,
+    }),
+    filesize(),
+  ],
 };
