91763dbfe6416098f6e55c74220f36661659199d
yunji <yunji.me@outlook.com>
2021-09-22 21:17:38 +0800

    feat: choropleth add labelLayer

--- packages/l7plot/src/plots/choropleth/index.ts
+++ packages/l7plot/src/plots/choropleth/index.ts
@@ -3,6 +3,7 @@ import { Plot } from '../../core/plot';
 import { ChoroplethOptions } from './interface';
 import { DEFAULT_OPTIONS } from './constants';
 import { AreaLayer } from '../../layers/area-layer';
+import { TextLayer } from '../../layers/text-layer';
 import { ILegendOptions, Source } from '../../types';
 import { LayerGroup } from '../../core/layer/layer-group';
 
@@ -23,6 +24,11 @@ export class Choropleth extends Plot<ChoroplethOptions> {
   public polygonLayer!: AreaLayer;
 
   /**
+   * 标注图层
+   */
+  public labelLayer: TextLayer | undefined;
+
+  /**
    * 获取默认配置
    */
   protected getDefaultOptions(): Partial<ChoroplethOptions> {
@@ -39,6 +45,11 @@ export class Choropleth extends Plot<ChoroplethOptions> {
     });
     const layerGroup = new LayerGroup([this.polygonLayer]);
 
+    if (this.options.label) {
+      this.labelLayer = this.createLabelLayer(source, this.options.label);
+      layerGroup.addlayer(this.labelLayer);
+    }
+
     return layerGroup;
   }
 
@@ -48,6 +59,19 @@ export class Choropleth extends Plot<ChoroplethOptions> {
   protected updateLayers(options: ChoroplethOptions) {
     const polygonLayerConfig = pick<any>(options, AreaLayer.LayerOptionsKeys);
     this.polygonLayer.updateOptions(polygonLayerConfig);
+
+    if (options.label) {
+      if (this.labelLayer) {
+        this.labelLayer.updateOptions({ ...options.label });
+      } else {
+        this.labelLayer = this.createLabelLayer(this.source, options.label);
+        this.layerGroup.addlayer(this.labelLayer);
+      }
+    } else {
+      if (this.labelLayer) {
+        this.layerGroup.removelayer(this.labelLayer);
+      }
+    }
   }
 
   /**
