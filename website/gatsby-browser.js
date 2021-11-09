fc3e184986109f3ae2b97f059e4682e8f9ae71ff
yunji <yunji.me@outlook.com>
2021-11-10 00:07:04 +0800

    docs: demo

--- website/gatsby-browser.js
+++ website/gatsby-browser.js
@@ -2,3 +2,5 @@ window.l7plot = require('@antv/l7plot');
 window.react = require('react');
 window.reactDom = require('react-dom');
 window.antd = require('antd');
+
+require('antd/lib/cascader/style/css');
