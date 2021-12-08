7a02620a479b42368e0f094ae1e636e9e2b696ed
lviser <yunji.me@outlook.com>
2021-12-08 22:42:29 +0800

    feat: export china boundary style (#106)

--- packages/l7plot/src/plots/choropleth/layer.ts
+++ packages/l7plot/src/plots/choropleth/layer.ts
@@ -1,13 +1,17 @@
+import { deepAssign } from '../../utils';
+import { ChinaBoundaryStyle, ChoroplethOptions } from './types';
 import { PathLayer } from '../../layers/path-layer';
-import { PlotLayerOptions } from '../../types';
 import { CHINA_BOUNDARY_STYLE } from './constants';
 
-export const createCountryBoundaryLayer = (data: any, plotLayerConfig?: PlotLayerOptions) => {
-  const { visible, minZoom, maxZoom, zIndex = 0 } = plotLayerConfig || {};
+export const createCountryBoundaryLayer = (data: any, plotConfig?: ChoroplethOptions) => {
+  const { visible, minZoom, maxZoom, zIndex = 0, chinaBorder } = plotConfig || {};
+  const borderStyle: Required<ChinaBoundaryStyle> =
+    typeof chinaBorder === 'object' ? deepAssign({}, CHINA_BOUNDARY_STYLE, chinaBorder) : CHINA_BOUNDARY_STYLE;
   const chinaBoundaryFeatures = data.features.filter(({ properties }) =>
     ['coast', 'hkm', 'national'].includes(properties.type)
   );
   const disputeBoundaryFeatures = data.features.filter(({ properties }) => properties.type === 'dispute');
+
   const chinaBoundaryLayer = new PathLayer({
     name: 'chinaBoundaryLayer',
     visible,
@@ -21,17 +25,17 @@ export const createCountryBoundaryLayer = (data: any, plotLayerConfig?: PlotLaye
     color: {
       field: 'type',
       value: ({ type }) => {
-        return CHINA_BOUNDARY_STYLE[type].color;
+        return borderStyle[type].color;
       },
     },
     size: {
       field: 'type',
       value: ({ type }) => {
-        return CHINA_BOUNDARY_STYLE[type].width;
+        return borderStyle[type].width;
       },
     },
     style: {
-      opacity: ['type', (type) => CHINA_BOUNDARY_STYLE[type].opacity],
+      opacity: ['type', (type) => borderStyle[type].opacity],
     },
   });
   const chinaDisputeBoundaryLayer = new PathLayer({
@@ -44,12 +48,12 @@ export const createCountryBoundaryLayer = (data: any, plotLayerConfig?: PlotLaye
       data: { type: 'FeatureCollection', features: disputeBoundaryFeatures },
       parser: { type: 'geojson' },
     },
-    color: CHINA_BOUNDARY_STYLE['dispute'].color,
-    size: CHINA_BOUNDARY_STYLE['dispute'].width,
+    color: borderStyle.dispute.color,
+    size: borderStyle.dispute.width,
     style: {
-      opacity: CHINA_BOUNDARY_STYLE['dispute'].opacity,
+      opacity: borderStyle.dispute.opacity,
       lineType: 'dash',
-      dashArray: CHINA_BOUNDARY_STYLE['dispute'].dashArray as [number, number],
+      dashArray: borderStyle.dispute.dashArray as [number, number],
     },
   });
 
