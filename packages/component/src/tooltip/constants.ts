51154a49a23aaa6c5a7f72d0a94858cecffec74b
lviser <yunji.me@outlook.com>
2021-12-29 19:10:07 +0800

    docs: domstyle for component (#119)

--- packages/component/src/tooltip/constants.ts
+++ packages/component/src/tooltip/constants.ts
@@ -1,9 +1,9 @@
 export const CONTAINER_CLASS = 'l7plot-tooltip';
-export const TITLE_CLASS = 'l7plot-tooltip-title';
-export const LIST_CLASS = 'l7plot-tooltip-list';
-export const LIST_ITEM_CLASS = 'l7plot-tooltip-list-item';
-export const NAME_CLASS = 'l7plot-tooltip-name';
-export const VALUE_CLASS = 'l7plot-tooltip-value';
+export const TITLE_CLASS = 'l7plot-tooltip__title';
+export const LIST_CLASS = 'l7plot-tooltip__list';
+export const LIST_ITEM_CLASS = 'l7plot-tooltip__list-item';
+export const NAME_CLASS = 'l7plot-tooltip__name';
+export const VALUE_CLASS = 'l7plot-tooltip__value';
 
 export const CONTAINER_TPL = `<div class="${CONTAINER_CLASS}">
   <div class="${TITLE_CLASS}"></div>
