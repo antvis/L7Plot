27954fff4e433dc388f9d8811da42a10feb033cd
yunji <yunji.me@outlook.com>
2022-05-18 15:12:09 +0800

    test: layer group

--- packages/composite-layers/__tests__/unit/core/layer-group.test.ts
+++ packages/composite-layers/__tests__/unit/core/layer-group.test.ts
@@ -1,11 +1,14 @@
-import { PointLayer } from '@antv/l7-layers';
 import { LayerGroup } from '../../../src/core/layer-group';
+import { PointLayer } from '../../../src/core-layers/point-layer';
 
 describe('layer group', () => {
   const layerGroup = new LayerGroup([]);
-  const pointLayer1 = new PointLayer({ name: 'pointLayer' });
-  const pointLayer2 = new PointLayer({});
-  const pointLayer3 = new PointLayer({});
+  const pointLayer1 = new PointLayer({
+    name: 'pointLayer',
+    source: { data: [], parser: { type: 'json', x: 'x', y: 'y' } },
+  });
+  const pointLayer2 = new PointLayer({ source: { data: [], parser: { type: 'json', x: 'x', y: 'y' } } });
+  const pointLayer3 = new PointLayer({ source: { data: [], parser: { type: 'json', x: 'x', y: 'y' } } });
 
   it('addLayer', () => {
     layerGroup.addLayer(pointLayer1);
@@ -18,7 +21,7 @@ describe('layer group', () => {
   it('getLayer', () => {
     expect(layerGroup.getLayers().length).toEqual(3);
     expect(layerGroup.getLayerByName(pointLayer1.name)).toEqual(pointLayer1);
-    expect(layerGroup.getLayer(pointLayer2.id)).toEqual(pointLayer2);
+    expect(layerGroup.getLayer(pointLayer2.layer.id)).toEqual(pointLayer2);
   });
 
   // eslint-disable-next-line jest/no-commented-out-tests
@@ -33,7 +36,7 @@ describe('layer group', () => {
     expect(layerGroup.removeLayer(pointLayer1)).toBeTruthy();
     expect(layerGroup.hasLayer(pointLayer1)).toBeFalsy();
 
-    expect(layerGroup.removeLayer(pointLayer2.id)).toBeTruthy();
+    expect(layerGroup.removeLayer(pointLayer2.layer.id)).toBeTruthy();
     expect(layerGroup.hasLayer(pointLayer2)).toBeFalsy();
 
     layerGroup.removeAllLayer();
