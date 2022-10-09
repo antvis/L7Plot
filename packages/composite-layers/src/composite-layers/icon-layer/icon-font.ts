418ecf51dbcba6cf728b50f1bb347b08d136c19c
象数 <zhengxue.lzx@antgroup.com>
2022-10-09 15:57:42 +0800

    feat: iconfont 加载完成时初始化图层

--- packages/composite-layers/src/composite-layers/icon-layer/icon-font.ts
+++ packages/composite-layers/src/composite-layers/icon-layer/icon-font.ts
@@ -1,12 +1,31 @@
 import { IconFontLayerOptions } from './types';
+import { Scene } from '../../types';
 import { DEFAULT_OPTIONS } from './constants';
 import { IconLayer } from './icon';
+import { CompositeLayerEvent } from '../../core/constants';
 
 export class IconFontLayer extends IconLayer<IconFontLayerOptions> {
+  private fontLoaded = true;
   /**
    * 默认配置项
    */
   static DefaultOptions = DEFAULT_OPTIONS;
+  /**
+   * 添加到场景
+   */
+  public addTo(scene: Scene) {
+    this.scene = scene;
+    this.initAssets();
+    if (this.fontLoaded) {
+      this.subLayers.addTo(scene);
+      this.emit(CompositeLayerEvent.ADD);
+    } else {
+      this.on('fontloaded', () => {
+        this.subLayers.addTo(scene);
+        this.emit(CompositeLayerEvent.ADD);
+      });
+    }
+  }
 
   /**
    * 初始化资源
@@ -16,22 +35,20 @@ export class IconFontLayer extends IconLayer<IconFontLayerOptions> {
   }
 
   /**
-   * load 图片资源
+   * load 字体资源
    */
   protected loadIconFontAtlas() {
     const { fontFamily, fontPath, iconFonts } = this.options.iconAtlas;
     const scene = this.scene;
-    scene?.addFontFace(fontFamily, fontPath);
-    scene?.addIconFonts(iconFonts);
-  }
+    if (fontFamily && fontPath) {
+      this.fontLoaded = false;
+      scene?.once('fontloaded', (e) => {
+        this.emit('fontloaded', e);
+        this.fontLoaded = true;
+      });
+      scene?.addFontFace(fontFamily, fontPath);
+    }
 
-  /**
-   * load 图片资源
-   */
-  protected addIconFontAtlas(iconAtlas: IconFontLayerOptions['iconAtlas']) {
-    const { fontFamily, fontPath, iconFonts } = iconAtlas;
-    const scene = this.scene;
-    scene?.addFontFace(fontFamily, fontPath);
     scene?.addIconFonts(iconFonts);
   }
 }
