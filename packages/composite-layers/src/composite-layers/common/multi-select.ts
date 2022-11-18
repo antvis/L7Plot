dd9655655ce5c16f7162631ca9b1d63d55f39eac
yunji <yunji.me@outlook.com>
2022-11-18 15:54:29 +0800

    fix: 修复复合图层多选触发条件

--- packages/composite-layers/src/composite-layers/common/multi-select.ts
+++ packages/composite-layers/src/composite-layers/common/multi-select.ts
@@ -4,7 +4,7 @@ import { isPressing } from '../../utils';
  * 是否开启手动（鼠标加快捷键）控制多选
  */
 export const isGestureMultiSelect = (enabledMultiSelect?: boolean, triggerMultiSelectKey?: KeyboardEvent['key']) => {
-  const isMultiSelect = enabledMultiSelect && triggerMultiSelectKey ? isPressing(triggerMultiSelectKey) : true;
+  const isMultiSelect = enabledMultiSelect && (triggerMultiSelectKey ? isPressing(triggerMultiSelectKey) : true);
 
   return isMultiSelect;
 };
