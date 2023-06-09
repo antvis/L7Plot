1a5829b3bcb181684d9164f5f5a2b957e99e8cb2
yanxiong <oujinhui.ojh@antgroup.com>
2023-06-09 11:21:04 +0800

    fix: 修复依赖和导出问题

--- packages/composite-layers/src/index.ts
+++ packages/composite-layers/src/index.ts
@@ -8,6 +8,7 @@ export * from './composite-layers/choropleth-layer/types';
 // 标注图层及类型定义 | author by [lzxue]](https://github.com/lzxue)
 export { IconFontLayer, IconImageLayer } from './composite-layers/icon-layer';
 export * from './composite-layers/icon-layer/types';
+// 客流聚合图层及类型定义 | author by [heiyexing]](https://github.com/heiyexing)
 export { TrafficFlowLayer } from './composite-layers/traffic-flow-layer';
 export * from './composite-layers/traffic-flow-layer/types';
 // 热力图层及类型定义 | author by [yunji]](https://github.com/lvisei)
