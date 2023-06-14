477fc5ee80d38b79dc3086343479d6067eccf79d
yanxiong <oujinhui.ojh@antgroup.com>
2023-06-14 14:22:08 +0800

    refactor: 去除对 d3-array 的依赖

--- packages/composite-layers/src/composite-layers/flow-layer/utils/zoom.ts
+++ packages/composite-layers/src/composite-layers/flow-layer/utils/zoom.ts
@@ -1,10 +1,8 @@
-import { bisectLeft } from 'd3-array';
+import { minBy } from 'lodash-es';
 
 export function findAppropriateZoom(availableZoomLevels: number[], zoom: number) {
   if (!availableZoomLevels.length) {
     throw new Error('No available zoom levels');
   }
-  return availableZoomLevels[
-    Math.min(bisectLeft(availableZoomLevels, Math.floor(zoom)), availableZoomLevels.length - 1)
-  ];
+  return minBy(availableZoomLevels, (availableZoom) => Math.abs(availableZoom + 1 - zoom));
 }
