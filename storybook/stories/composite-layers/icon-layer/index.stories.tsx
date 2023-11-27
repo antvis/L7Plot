8223d5da36fa396b12da22c59041d30fd90d21ae
yunji <yunji.me@outlook.com>
2023-11-27 16:39:35 +0800

    chore: iconImageScale

--- storybook/stories/composite-layers/icon-layer/index.stories.tsx
+++ storybook/stories/composite-layers/icon-layer/index.stories.tsx
@@ -1,8 +1,10 @@
 import { storiesOf } from '@storybook/react';
 
 import IconImage from './iconImage';
+import IconImageScale from './iconImageScale';
 import IconFont from './IconFont';
 
 storiesOf('复合图层/标注图层', module)
   .add('图片标注', () => <IconImage />)
+  .add('图片自定义映射', () => <IconImageScale />)
   .add('图标标注', () => <IconFont />);
